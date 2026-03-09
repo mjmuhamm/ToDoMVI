b3baf623279250dac25252f97ec3a66e29177a74
mjmuhamm <53790225+mjmuhamm@users.noreply.github.com>
2026-03-09 18:43:18 -0400

    additions

--- composeApp/src/commonMain/kotlin/com/example/todomvi/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/todomvi/App.kt
@@ -4,14 +4,18 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -19,6 +23,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -39,7 +45,7 @@ fun App() {
         val viewModel = viewModel<ToDoViewModel>(factory = toDoModelFactory)
         Column(
             modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
+                .background(Color.White)
                 .safeContentPadding()
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -56,34 +62,73 @@ fun App() {
                     modifier = Modifier.fillMaxWidth(),
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    if (state.value.isLoading) {
-                        CircularProgressIndicator()
-                    }
 
-                    Column {
-                        Button(onClick = { viewModel.processIntent(MainIntent.FetchData) }) {
+
+                    Column(Modifier.padding(16.dp)) {
+                        Button(onClick = { viewModel.processIntent(MainIntent.FetchData) }, shape = RoundedCornerShape(6.dp),
+                            modifier = Modifier.width(130.dp)) {
                             Text("Get Data")
                         }
 
                         Spacer(modifier = Modifier.height((8.dp)))
-                        Button(onClick = { viewModel.processIntent(MainIntent.ClearData) }) {
+                        Button(onClick = { viewModel.processIntent(MainIntent.ClearData) }, shape = RoundedCornerShape(6.dp),
+                            modifier = Modifier.width(130.dp)) {
                             Text("Clear Data")
                         }
 
                         Spacer(modifier = Modifier.height((8.dp)))
-                        Button(onClick = { viewModel.processIntent(MainIntent.RefreshData) }) {
+                        Button(onClick = { viewModel.processIntent(MainIntent.RefreshData) }, shape = RoundedCornerShape(6.dp),
+                            modifier = Modifier.width(130.dp)) {
                             Text("Refresh Data")
                         }
                     }
 
-                    Spacer(modifier = Modifier.height((8.dp)))
 
+
+                    Spacer(modifier = Modifier.height((8.dp)))
+                    if (state.value.isLoading) {
+                        CircularProgressIndicator()
+                    }
 
                     LazyColumn {
                         items(state.value.items) { item ->
-                            Text("Name: ${item.name}", modifier = Modifier.padding(8.dp) )
+
+                            Row(Modifier.padding(start = 8.dp, top = 8.dp)) {
+                                Text("Name:")
+                                Text(
+                                    item.name,
+                                    modifier = Modifier.padding(start = 8.dp),
+                                    fontWeight = FontWeight.Bold
+                                )
+                            }
+                            Row(Modifier.padding(start = 8.dp)) {
+                                Text("ID:")
+                                Text(
+                                    "${item.id}",
+                                    modifier = Modifier.padding(start = 8.dp),
+                                )
+                            }
+                            Row(Modifier.padding(start = 8.dp)) {
+                                Text("Email:")
+                                Text(
+                                    item.email,
+                                    modifier = Modifier.padding(start = 8.dp)
+                                )
+                            }
+                            Row(Modifier.padding(start = 8.dp)) {
+                                Text("Address:")
+                                Text(
+                                    "${item.address.street}, ${item.address.city}",
+                                    modifier = Modifier.padding(start = 8.dp)
+                                )
+                            }
+                            Spacer(modifier = Modifier.height((4.dp)))
+
+
                         }
                     }
+
+
                 }
             }
         }
